4595ee9d6c9d8d5313cf1aeec910e6e3caf05aa0
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-09-01 11:24:22 -0400

    started menu

---
+++ shoeresale/src/components/ Menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class  Menu extends Component {
+    render() {
+        return (
+            <div>
+                <h3>logo</h3>
+                <a href="##">Home</a>
+                <a href="##">about</a>
+                <input type='text' value=''></input>
+            </div>
+        )
+    }
+}
