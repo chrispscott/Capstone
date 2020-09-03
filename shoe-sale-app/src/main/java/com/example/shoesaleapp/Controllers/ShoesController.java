63961aefa84a210c6ed49dfc8350df360f4764b8
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-09-03 19:08:23 -0400

    this is a commit

--- shoe-sale-app/src/main/java/com/example/shoesaleapp/Controllers/ShoesController.java
+++ shoe-sale-app/src/main/java/com/example/shoesaleapp/Controllers/ShoesController.java
@@ -1,4 +1,21 @@
 package com.example.shoesaleapp.Controllers;
 
+import com.example.shoesaleapp.models.Shoes;
+import com.example.shoesaleapp.repositories.ShoesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Shoes")
 public class ShoesController {
+
+    @Autowired
+    private ShoesService shoesService;
+
+    @GetMapping(“/Shoes”)
+    public Iterable<Shoes> listShoes() {
+        return ShoesService.listShoes();
+    }
+
 }
