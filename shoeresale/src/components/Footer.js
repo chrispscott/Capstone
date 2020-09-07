0255dcfb4658b23922b5a0a840dbf57bc437d6b7
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-09-07 03:34:37 -0400

    this is a commit

--- shoeresale/src/components/Footer.js
+++ shoeresale/src/components/Footer.js
@@ -5,11 +5,11 @@ const Footer = () => {
         <footer>
             <h1>FOLLOW US ON SOCIAL MEDIA</h1>
             <div className="social-media">
-                <i class="fa fa-twitter" aria-hidden="true"></i>
-                <i class="fa fa-youtube-play" aria-hidden="true"></i>
-                <i class="fa fa-instagram" aria-hidden="true"></i>
-                <i class="fa fa-facebook-square" aria-hidden="true"></i>
-                <i class="fa fa-whatsapp" aria-hidden="true"></i>
+               <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+               <a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+               <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+               <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+               <a href="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
             </div>
         </footer>
     );
