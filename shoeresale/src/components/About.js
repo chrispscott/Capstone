9516258b347c4892279e17e954d404eb33393e1f
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-09-07 02:35:43 -0400

    this is a commit

--- shoeresale/src/components/About.js
+++ shoeresale/src/components/About.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Menu from "./ Menu";
+import Banner from "./Banner";
 
 function About(props) {
-    return (<div className="container">
-        {/* < Menu /> */}
-            <p className="about-box">
-                shoe resale is a website dedicated to helping sneaker and shoe  enthusiast share ideas and resale unwanted items and shoes , cleaning products and accessories. Users add a list of items that they would like to sell list their email so people can contact them and the prices for all the items .
-            </p>
+    return (<div>
+            < Banner />
+            <div className="container">
+            
+                <p className="about-box">
+                    shoe resale is a website dedicated to helping sneaker and shoe  enthusiast share ideas and resale unwanted items and shoes , cleaning products and accessories. Users add a list of items that they would like to sell list their email so people can contact them and the prices for all the items .
+                </p>
+            </div>
         </div>
     );
 }
