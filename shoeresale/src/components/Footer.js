3422d0433e955c252a990d30b673b3d7a6069090
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-09-07 03:10:43 -0400

    this is a commit

--- shoeresale/src/components/Footer.js
+++ shoeresale/src/components/Footer.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            footer
+            <h1>FOLLOW US ON SOCIAL MEDIA</h1>
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+            <i class="fa fa-youtube-play" aria-hidden="true"></i>
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+            <i class="fa fa-facebook-square" aria-hidden="true"></i>
+            <i class="fa fa-whatsapp" aria-hidden="true"></i>
         </div>
     );
 };
